bf494d1b8e7ba1441edf4adcc940e360d6040114
NguyenTrungMaxBka <trungnv@bkacad.edu.vn>
2020-12-15 16:20:36 +0700

    add new page

---
+++ pages/bao-cao-tien-do/index.js
@@ -0,0 +1,15 @@
+import Layout from '../../components/Layout'
+import Head from 'next/head'
+
+// Thứ tự thực hiện các hàm bất đồng bộ
+// 1 là getStaticPaths - hàm bất đồng bộ lấy ra danh sách các params động
+// 2 getStaticProps lấy ra các props tĩnh hoặc truy vấn dữ liệu từ bên ngoài sau đó vẫn trả ra props
+export default function Post({ postData }) {
+    return (
+        <Layout
+            code='PROGRESS_REPORT'
+            title='Báo cáo tiến độ'
+        >
+        </Layout>
+    )
+}
